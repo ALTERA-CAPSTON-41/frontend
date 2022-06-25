8c853d047bc3b2434a78735aecff3b9fd56ece41
Beby Avilla <bebyavilla@gmail.com>
2022-06-26 01:21:54 +0700

    update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,27 +4,23 @@
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
           <v-col cols="12" sm="8" md="8">
+            <div class="title">
+        <v-img
+            class="mx-auto"
+            max-height="350"
+            max-width="120"
+            justify-center
+            src="../assets/img/lego.png"
+        ></v-img>
+        <span>Hai, Admin</span>
+        </div>
             <v-card class="elevation-12">
               <v-row>
                 <v-col cols="12" md="8">
                   <v-card-text class="mt-12">
-                    <!-- <h1
-                          class="text-center display-2 teal--text text--accent-3"
-                        >Sign in to Diprella</h1> -->
                     <h2>Sign In</h2><br>
                     <p>Enter Your Name and Password</p><br><br>
-                    <!-- <div class="text-center mt-4">
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-facebook-f</v-icon>
-                          </v-btn>
 
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-google-plus-g</v-icon>
-                          </v-btn>
-                          <v-btn class="mx-2" fab color="black" outlined>
-                            <v-icon>fab fa-linkedin-in</v-icon>
-                          </v-btn>
-                        </div> -->
                     <v-form>
                       <h4>Username</h4>
                       <v-text-field
