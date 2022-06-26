280d6ae832438720c984228aace8797b8e60b53c
Beby Avilla <bebyavilla@gmail.com>
2022-06-26 22:22:08 +0700

    add v-banner

--- src/views/Beranda.vue
+++ src/views/Beranda.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>ini halaman beranda</h1>
+<v-banner>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus nec sem id malesuada.
+    Curabitur lacinia sem et turpis euismod, eget elementum ex pretium.
+    <template v-slot:actions>
+      <v-btn
+        text
+        color="primary"
+      >
+        Dismiss
+      </v-btn>
+      <v-btn
+        text
+        color="primary"
+      >
+        Retry
+      </v-btn>
+    </template>
+  </v-banner>
 </template>
