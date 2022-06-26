8aaafbc3331c5e39d2d47ee75de1ab479a7a3878
Beby Avilla <bebyavilla@gmail.com>
2022-06-26 23:00:46 +0700

    update login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,27 @@
 <template>
   <v-app id="inspire">
-    <v-content>
+    <v-main>
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="8">
-            <v-card class="elevation-12">
+          <v-col cols="12" sm="8" md="10">
+            <v-card class="elevation-5">
               <v-row>
-                <v-col cols="12" md="8">
-                  <v-card-text class="mt-12">
-                    <h2>Sign In</h2><br>
-                    <p>Enter Your Name and Password</p><br><br>
+                <v-col cols="12" md="7">
+                  <br>
+                <v-img
+                  class="mx-auto"
+                  height="150"
+                  width="150"
+                  src="../assets/img/loginnn.png"
+                ></v-img>
+                  <v-card-text class="mt-4">
+                    <h1>Sign In</h1><br>
+                    <p>Enter Your Name and Password</p><br>
 
                     <v-form>
                       <h4>Username</h4>
                       <v-text-field
+                        prepend-inner-icon="mdi-account"
                         label="Enter Your Username"
                         name="Email"
                         type="text"
@@ -24,11 +32,13 @@
                       />
                       <h4>Password</h4>
                       <v-text-field
+                      prepend-inner-icon="mdi-lock"
                         id="password"
                         label="Enter Your Password"
                         name="password"
                         type="password"
                         color="teal accent-3"
+                        append-icon="mdi-eye-off"
                         filled
                         rounded
                         dense
@@ -36,16 +46,16 @@
                     </v-form>
                   </v-card-text>
                   <div class="text-center mt-5">
-                    <v-btn v-on:click="login" width="300px" height="45px" rounded color="teal accent-3" dark>SIGN IN</v-btn>
+                    <v-btn v-on:click="login" width="300px" height="45px" rounded color="#819c76" dark>SIGN IN</v-btn>
                   </div>
                 </v-col>
-                <v-col cols="12" md="4" class="teal accent-3"></v-col>
+                <v-col cols="12" md="5" class="green lighten-3"></v-col>
              </v-row>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
